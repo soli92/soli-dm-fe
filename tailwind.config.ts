ae9e2d0c10f9fb6169925b6208f83f698c3df00d
soli92 <45817292+soli92@users.noreply.github.com>
2026-04-02 16:36:39 +0200

    feat: add Tailwind configuration with solids preset

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  presets: [require("@soli92/solids/tailwind-preset")],
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
