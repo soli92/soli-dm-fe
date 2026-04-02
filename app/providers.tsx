5e2af518b85d0a44332f7adcf5a49a4da68a3d4a
soli92 <45817292+soli92@users.noreply.github.com>
2026-04-02 16:38:07 +0200

    feat: add theme and toast providers

---
+++ app/providers.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "sonner";
+import React from "react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="data-theme"
+      defaultTheme="light"
+      enableSystem
+      themes={["light", "dark", "fantasy", "cyberpunk", "90s-party"]}
+      storageKey="soli-dm-theme"
+    >
+      {children}
+      <Toaster position="top-right" />
+    </ThemeProvider>
+  );
+}
